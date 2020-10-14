3661aa09bcc7739695ecc7aa698f3ab811929c60
oyjjpp <ouyangjuncust@aliyun.com>
2020-10-14 20:59:56 +0800

    learn vue

---
+++ src/components/ShowHTML.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <lable>直接输出的模式：</lable>
+    <div>{{msg}}</div>
+    <lable>解析后输出的模式：</lable>
+    <div v-html="msg"></div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      msg: '<div stly="font-size:30px;color:red">helloWorld</div>'
+    }
+  }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import VueResource from 'vue-resource'
+import HelloWorld from '@/components/HelloWorld'
+import ShowText from '@/components/ShowText'
+import ShowHTML from '@/components/ShowHTML'
+import JSExpressionTest from '@/components/JSExpressionTest'
+
+Vue.use(Router)
+Vue.use(VueResource)
+
+// const routes = [
+//   {
+//     path: '/',
+//     component: ressolve => require(['../pages/index'],  resolve),
+//     meta: {
+//       title: 'home'
+//     }
+//   },
+// ]
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/ShowText',
+      component: ShowText
+    },
+    {
+      path: '/ShowHtml',
+      component: ShowHTML
+    },
+    {
+      path: '/JSExpression',
+      component: JSExpressionTest
+    }
+  ]
+})
